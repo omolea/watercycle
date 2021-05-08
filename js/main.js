053b78218d5660af5b19f4bd4b68aafe26269129
omolea <63256641+omolea@users.noreply.github.com>
2021-05-08 19:08:16 +0300

    week 3
    Assignments

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function updateCurrentDate() {
+    let options = {
+        weekday: "long"
+        ,day: "numeric"
+        ,month: "long"
+        ,year: "numeric"
+        ,hour: "numeric"
+        ,minute: "numeric"
+    };
+    // return new Date();
+    return new Date().toLocaleDateString("en-Us", options)
+}
